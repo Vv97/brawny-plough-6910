3af7d81ea85cec8ab4c51731ddfabe2a1fcf5bf0
Vv97 <VishalVarma537@gmail.com>
2023-03-30 12:34:43 +0530

    package install axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,25 @@ import './App.css';
 import { AllRoutes } from './Routes/AllRoutes';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/> */}
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <AllRoutes />
       {/* <Footer/> */}
     </div>
