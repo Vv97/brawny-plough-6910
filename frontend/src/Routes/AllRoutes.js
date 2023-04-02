70d71b29bbd8809187be87ad12ffb1074e828a0c
satya M <satya2maity@gmail.com>
2023-04-02 16:03:01 +0530

    added AllRoutest

--- frontend/src/Routes/AllRoutes.js
+++ frontend/src/Routes/AllRoutes.js
@@ -6,19 +6,22 @@ import AdminPage from "../pages/Admin/MainPage/AdminPage";
 import { Register } from "../pages/register/Register";
 import { Login } from "../pages/login/Login";
 import CartItem from "../pages/CartItem/CartItem";
-import AdminLogin from "../pages/Admin/MainPage/AdminLogin";
+import Address from "../pages/Address/Address";
+// import AdminLogin from "../pages/Admin/MainPage/AdminLogin";
 
 
 export const AllRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<CartItem />} />
             <Route path="/products" element={<Products />} />
             <Route path="/admin" element={<AdminPage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/cart" element={<CartItem />} /> */}
-            <Route path="/adminlogin" element={<AdminLogin/>} />
+            <Route path="/cart/address" element={<Address/>} />
+            
+       <Route path="/cart" element={<CartItem />} /> 
+            {/* <Route path="/adminlogin" element={<AdminLogin/>} /> */}
         </Routes>
     );
 };
